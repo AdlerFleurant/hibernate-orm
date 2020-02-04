6cec68e76c3101fc2677e564643c644a4164ed18
Adler Fleurant <Adler.Fleurant@delta.com>
2020-02-04 09:50:11 -0500

    Fixing groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,10 +1,12 @@
-pipeline{
-    agent{label 'maven'}
-    
-    stages{
-        stage('Build Hibernate'){
+pipeline {
+    agent { label 'maven' }
+
+    stages {
+        stage('Build Hibernate') {
             steps {
-                sh "./gradlew --no-daemon --system-prop 'org.gradle.jvmargs=-Xmx${env.XMX} -XX:MaxMetaspaceSize=512m' clean build"
+                script {
+                    sh "./gradlew --no-daemon --system-prop 'org.gradle.jvmargs=-Xmx${env.XMX} -XX:MaxMetaspaceSize=512m' clean build"
+                }
             }
         }
     }
